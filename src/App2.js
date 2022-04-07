acedb03d6cf3a9119eda924b1113378a36daad95
kovacsborka <kovacsborbalaterez@gmail.com>
2022-04-07 17:06:50 +0200

    omfg

---
+++ src/App2.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+
+function App2() {
+  
+  return (
+    <>
+      <div>
+
+      </div>
+      <div>
+        <h1>Hi there!</h1>
+        <h3>this is my first react app</h3>
+        <img src="https://i.pinimg.com/originals/59/54/b4/5954b408c66525ad932faa693a647e3f.jpg" alt="" />
+      </div>
+    </>
+  );
+}
+
+export default App2;
